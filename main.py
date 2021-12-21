c542657f3f0a4bd7d93aa64b2b6d84d0aac6a771
jeswanthmukesh20 <jeswanthmukesh69@gmail.com>
2021-12-22 00:51:00 +0530

    debugged code

--- main.py
+++ main.py
@@ -10,15 +10,32 @@ bot_token = "5049973042:AAG--t9oSTHxO3DE30PyxXTItj98np26kls"
 bot = Bot(bot_token)
 
 __api  = f"https://api.telegram.org/bot{bot_token}/getUpdates"
+print(__api)
 
 group_name = "Cricket score"
 
 response = requests.get(url=__api).json()
+print("response")
 result = response['result']
 print(result)
 group_id = None
+def chat(update_id):
+    if update_id['message']['chat']:
+        if update_id['message']['chat']['title'] == group_name:
+            group_id = update_id['message']['chat']['id']
+            print(group_id)
+            return group_id
+        else:
+            return None
+    else:
+        return None
+
 for update_id in result:
-    chat_number = update_id["my_chat_member"] if "my_chat_member" in update_id else None
+    chat_number = update_id["my_chat_member"] if "my_chat_member" in update_id else chat(update_id)
+    if type(chat_number) is int:
+        group_id = chat_number
+        print("loop breaked")
+        break
     print(chat_number)
     if chat_number is None:
         continue
@@ -41,7 +58,7 @@ def overview():
     table = soup.find_all('li', attrs = {'class': 'cb-col cb-col-25 cb-mtch-blk cb-vid-sml-card-api videos-carousal-item cb-carousal-item-large cb-view-all-ga'})
     
     # replace this with the team initials eg. IND for India
-    pref = 'ENG'
+    pref = 'JKS'
     match = None
     for li in table:
         match = li.text.strip()
@@ -54,8 +71,8 @@ def overview():
             # print('\n')
             match =  match
             break
-    position = match.find("AUS")
-    position2 = match.find("ENG")
+    position = match.find("JKS")
+    position2 = match.find("DMG")
     final_score = ""
     for index, val in enumerate(match):
         if index - 3 == position:
@@ -67,12 +84,16 @@ def overview():
     return score
 
 
-score = overview()
+
 
 previous_msg = ""
+print(True) if group_id is not None else print(False)
 while group_id is not None:
-    previous_msg = overview()
-    if previous_msg != overview():
+    score = overview()
+    print(score)
+    print(overview())
+    if score != previous_msg:
         updater.bot.sendMessage(chat_id=f'{group_id}', text=score)
         print("Message is updated")
+        previous_msg = score
     sleep(1)
